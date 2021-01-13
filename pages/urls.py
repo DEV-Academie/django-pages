0da7039b366ee2c6c0382f1f68d84976264e6480
Erwin Matijsen <erwin@academie.dev>
2021-01-13 20:14:18 +0100

    Opdracht 1: voeg extra view toe

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pages.views import homepage
+from pages.views import hello, homepage
 
 urlpatterns = [
     path("", homepage, name="home"),
+    path("hello/", hello, name="hello"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def homepage(request):
     return HttpResponse("Hello, World!")
+
+
+def hello(request):
+    return HttpResponse("Hello, Class!")
